566efae8c7b6524e42f200617ec058df5450f68b
JainiSolanki <23cs096@charusat.edu.in>
2026-01-21 23:26:14 +0530

    test backend server locally

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("AadhaarQMS Backend is running");
+});
+
+module.exports = app;
